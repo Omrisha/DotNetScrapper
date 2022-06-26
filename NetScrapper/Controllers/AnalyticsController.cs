5d2447c243f6e78270ab373861533c828c5d8b5d
Omri Shapira <omri.shapira@projectverte.com>
2022-06-26 16:29:53 +0300

    add project

---
+++ NetScrapper/Controllers/AnalyticsController.cs
@@ -0,0 +1,138 @@
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+using NetScrapper.Models;
+using System.Globalization;
+using System.Net;
+using System.Xml.Linq;
+
+namespace NetScrapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnalyticsController : ControllerBase
+    {
+        readonly CultureInfo culture = new("en-US");
+        private readonly MyDbContext _dbContext;
+        private readonly IConfiguration _configuration;
+
+        public AnalyticsController(MyDbContext dbContext, IConfiguration configuration)
+        {
+            _dbContext = dbContext;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("[action]/{authorId}")]
+        public async Task<ActionResult> CreatePosts(string authorId)
+        {
+            try
+            {
+                XDocument doc = XDocument.Load("https://www.c-sharpcorner.com/members/" + authorId + "/rss");
+                if (doc == null)
+                    return BadRequest("Invalid Author Id");
+
+                var entries = from item in doc.Root.Descendants().First(i => i.Name.LocalName == "channel").Elements().Where(i => i.Name.LocalName == "item")
+                              select new Feed
+                              {
+                                  Content = item.Elements().First(i => i.Name.LocalName == "description").Value,
+                                  Link = (item.Elements().First(i => i.Name.LocalName == "link").Value).StartsWith("/") ? "https://www.c-sharpcorner.com" + item.Elements().First(i => i.Name.LocalName == "link").Value : item.Elements().First(i => i.Name.LocalName == "link").Value,
+                                  PubDate = Convert.ToDateTime(item.Elements().First(i => i.Name.LocalName == "pubDate").Value, culture),
+                                  Title = item.Elements().First(i => i.Name.LocalName == "title").Value,
+                                  FeedType = (item.Elements().First(i => i.Name.LocalName == "link").Value).ToLowerInvariant().Contains("blog") ? "Blog" : (item.Elements().First(i => i.Name.LocalName == "link").Value).ToLowerInvariant().Contains("news") ? "News" : "Article",
+                                  Author = item.Elements().First(i => i.Name.LocalName == "author").Value
+                              };
+
+                List<Feed> feeds = entries.OrderByDescending(o => o.PubDate).ToList();
+                string urlAddress = string.Empty;
+                List<ArticleMatrix> articleMatrices = new();
+                _ = int.TryParse(_configuration["ParallelTasksCount"], out int parallelTasksCount);
+
+                Parallel.ForEach(feeds, new ParallelOptions { MaxDegreeOfParallelism = parallelTasksCount }, async feed =>
+                {
+                    urlAddress = feed.Link;
+
+                    var httpClient = new HttpClient
+                    {
+                        BaseAddress = new Uri(urlAddress)
+                    };
+                    var result = httpClient.GetAsync("").Result;
+
+                    string strData = "";
+
+                    if (result.StatusCode == HttpStatusCode.OK)
+                    {
+                        strData = await result.Content.ReadAsStringAsync();
+
+                        HtmlDocument htmlDocument = new();
+                        htmlDocument.LoadHtml(strData);
+
+                        ArticleMatrix articleMatrix = new()
+                        {
+                            AuthorId = authorId,
+                            Author = feed.Author,
+                            Link = feed.Link,
+                            Title = feed.Title,
+                            Type = feed.FeedType,
+                            PubDate = feed.PubDate,
+                        };
+
+                        string category = "Uncategorized";
+                        if (htmlDocument.GetElementbyId("ImgCategory") != null)
+                        {
+                            category = htmlDocument.GetElementbyId("ImgCategory").GetAttributeValue("title", "");
+                        }
+
+                        articleMatrix.Category = category;
+
+                        var view = htmlDocument.DocumentNode.SelectSingleNode("//span[@id='ViewCounts']");
+                        if (view != null)
+                        {
+                            articleMatrix.Views = view.InnerText;
+                            
+                            if (articleMatrix.Views.Contains('m'))
+                            {
+                                articleMatrix.ViewsCount = decimal.Parse(articleMatrix.Views[0..^1]) * 1000000;
+                            }
+                            else if (articleMatrix.Views.Contains('k'))
+                            {
+                                articleMatrix.ViewsCount = decimal.Parse(articleMatrix.Views[0..^1]) * 1000;
+                            }
+                            else
+                            {
+                                _ = decimal.TryParse(articleMatrix.Views, out decimal viewsCount);
+                                articleMatrix.ViewsCount = viewsCount;
+                            }
+                        }
+                        else
+                        {
+                            articleMatrix.ViewsCount = 0;
+                        }
+
+                        var like = htmlDocument.DocumentNode.SelectSingleNode("//span[@id='LabelLikeCount']");
+                        if (like != null)
+                        {
+                            _ = int.TryParse(like.InnerText, out int likes);
+                            articleMatrix.Likes = likes;
+                        }
+
+                        articleMatrices.Add(articleMatrix);
+                    }
+                });
+
+                _dbContext.ArticleMatrices.RemoveRange(_dbContext.ArticleMatrices.Where(x => x.AuthorId == authorId));
+
+                foreach (ArticleMatrix articleMatrix in articleMatrices)
+                {
+                    await _dbContext.ArticleMatrices.AddAsync(articleMatrix);
+                }
+
+                await _dbContext.SaveChangesAsync();
+                return Ok(articleMatrices);
+            }
+            catch (Exception)
+            {
+                return BadRequest("Invalid Author Id / Unhadnled error. Please try again.");
+            }
+        }
+    }
+}
---
+++ NetScrapper/Models/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NetScrapper.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<ArticleMatrix>? ArticleMatrices { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
